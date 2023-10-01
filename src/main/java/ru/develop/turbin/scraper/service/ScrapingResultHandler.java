2b758b3b09ff5c12b4f9efd11e14d4b608a20f54
turbin <van_turbin@mail.ru>
2023-10-01 17:56:00 +0300

    feature: дополнительное ожидание при ошибке

--- src/main/java/ru/develop/turbin/scraper/service/ScrapingResultHandler.java
+++ src/main/java/ru/develop/turbin/scraper/service/ScrapingResultHandler.java
@@ -2,9 +2,11 @@ package ru.develop.turbin.scraper.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.develop.turbin.scraper.dao.CourtCaseRepository;
 import ru.develop.turbin.scraper.entity.ScrapingTaskEntity;
+import ru.develop.turbin.scraper.enums.ScrapingError;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +16,9 @@ public class ScrapingResultHandler {
     private final CourtCaseRepository courtCaseRepository;
     private final ScrapingTaskService scrapingTaskService;
     private final CaseScrapingLogDaoDecorator caseScrapingLogDaoDecorator;
+
+    @Value("${configuration.additional_awaiting_on_error:20000}")
+    private Long additionalAwaitingOnError;
 
     public void skipCaseScraping(ScrapingTaskEntity scrapingTaskEntity, String caseNumber, String error) {
         log.warn("Дело {} пропущено", caseNumber);
@@ -33,6 +38,12 @@ public class ScrapingResultHandler {
         }
 
         caseScrapingLogDaoDecorator.writeFailedLog(scrapingTaskEntity, caseId, error);
+
+        try {
+            Thread.sleep(additionalAwaitingOnError);
+        } catch (InterruptedException e) {
+            log.error(ScrapingError.THREAD_ERROR.getLogMessage(), caseNumber);
+        }
     }
 
     public void completeCaseScraping(ScrapingTaskEntity scrapingTaskEntity, Long caseId) {
