5dcbf674094e0cf5bac75c66d5631eb2a91e8797
Turbin <iturbin@phoenixit.ru>
2024-11-25 16:46:25 +0300

    version: 1.1.0

--- src/main/java/ru/develop/turbin/scraper/config/ChromeConfig.java
+++ src/main/java/ru/develop/turbin/scraper/config/ChromeConfig.java
@@ -14,45 +14,17 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(prefix = "configuration", name = "browser", havingValue = "chrome")
 public class ChromeConfig {
 
-    //todo разобраться с опциями
     @Bean
     public ChromeOptions chromeOptions() {
         ChromeOptions options = new ChromeOptions();
-  //      options.setBrowserVersion("129.0.6668.100");
         options.addArguments("--disable-blink-features=AutomationControlled");
- //       options.addArguments("download.default_directory=" + downloadPath);
-//        options.addArguments("--download.prompt_for_download=false");             // Optional: Disable the download prompt
-//        options.addArguments("--download.directory_upgrade=true");                // Optional: Allow Chrome to move files from the default download folder to the specified folder
-//        options.addArguments("safebrowsing.enabled=true");                      // Optional: Enable safe browsing
-//        //options.addArguments("--disable-infobars");                               // Хром контролируется ...
-        //options.addArguments("--disable-extensions");                           // Отключить расширения
-        //options.addArguments("--disable-plugins");                           // Отключить расширения
         options.addArguments("--disable-gpu");                                  // Disable GPU acceleration (if needed)
         options.addArguments("--start-maximized");
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-//        options.addArguments("--no-sandbox");                                   // Disable sandbox (Linux environments)
-//        options.addArguments("--disable-popup-blocking");                       // Disable popup blocking
-//        options.addArguments("pdfjs.disabled=true");
 
-//        options.setExperimentalOption("prefs", ImmutableMap.of(
-//                "plugins.always_open_pdf_externally", true,
-//                "download.default_directory", downloadPath
-//        ));
-//
         options.setExperimentalOption("prefs", ImmutableMap.of(
                 "plugins.always_open_pdf_externally", true
         ));
-
-//        options.addArguments("--disable-plugins", "--disable-extensions", "--disable-pdfjs");
-//        // Set up ChromeOptions to handle file download
-//        Map<String, Object> prefs = new HashMap<>();
-//        prefs.put("profile.default_content_settings.popups", 0);
-//        prefs.put("download.default_directory", downloadPath);
-//        options.setExperimentalOption("prefs", prefs);
-//        options.setExperimentalOption("prefs", ImmutableMap.of(
-//                "download.default_directory", downloadPath
-//        ));
-
         return options;
     }
 
