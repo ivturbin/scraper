52543d37d3b0625d7d67232e8c10d6dc17a56290
Turbin <iturbin@phoenixit.ru>
2025-01-20 18:44:51 +0300

    feature: download file with IOUtils

--- src/main/java/dev/turbin/scraper/service/files/JavaFileDownloader.java
+++ src/main/java/dev/turbin/scraper/service/files/JavaFileDownloader.java
@@ -4,18 +4,27 @@ import dev.turbin.scraper.dao.CaseEventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@ConditionalOnMissingBean(FileDownloader.class)
+@ConditionalOnProperty(prefix = "configuration", name = "download", havingValue = "java")
 @Slf4j
 public class JavaFileDownloader implements FileDownloader {
     private final CaseEventRepository caseEventRepository;
+    private final WebDriver driver;
 
     @Override
     public void download(String fileLink, Long eventId) {
@@ -25,8 +34,28 @@ public class JavaFileDownloader implements FileDownloader {
         String fileName = splittedLink[splittedLink.length - 1];
 
         try {
-            //todo видимо так загружать с арбитра не даёт капча
-            fileData = IOUtils.toByteArray(new URL(fileLink));
+            //todo капчу кажется удалось обойти, скачивается html и похоже, что в value у него лежит pdf
+
+            // Извлекаем заголовки из браузера
+            HttpURLConnection connection = (HttpURLConnection) new URL(fileLink).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36");
+            connection.setRequestProperty("Accept-Language", "en-US,en;q=0.9");
+            connection.setRequestProperty("Referer", driver.getCurrentUrl());
+            connection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+
+            // Добавляем cookies
+            Set<Cookie> cookies = driver.manage().getCookies();
+            String cookieHeader = cookies.stream()
+                    .map(cookie -> cookie.getName() + "=" + cookie.getValue())
+                    .collect(Collectors.joining("; "));
+            connection.setRequestProperty("Cookie", cookieHeader);
+
+            // Читаем файл
+            // todo стрим нужно закрывать?
+            InputStream inputStream = connection.getInputStream();
+
+            fileData = IOUtils.toByteArray(inputStream);
+
         } catch (IOException e) {
             log.error("Ошибка чтения файла: {}", fileName);
             return;
