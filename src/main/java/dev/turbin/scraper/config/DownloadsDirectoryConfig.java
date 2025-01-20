5412fc70ebaad1300b5959febe92b5155f67795d
Turbin <iturbin@phoenixit.ru>
2025-01-20 22:05:29 +0300

    feature: add downloads directory configuration

--- src/main/java/dev/turbin/scraper/config/DownloadsDirectoryConfig.java
+++ src/main/java/dev/turbin/scraper/config/DownloadsDirectoryConfig.java
@@ -1,6 +1,8 @@
 package dev.turbin.scraper.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +13,26 @@ import java.io.File;
 public class DownloadsDirectoryConfig {
 
     @Bean("downloadsDirectory")
-    public String downloadsDirectory() {
-        String downloadsDirectory = System.getProperty("user.home") + File.separator + "kad" + File.separator + "tmp";
-        log.info("downloadsDirectory: {}", downloadsDirectory);
-        return downloadsDirectory;
+    @ConditionalOnProperty(prefix = "configuration.download", name = "directory", havingValue = "home")
+    public String downloadToHome() {
+        return withLogging(System.getProperty("user.home") + File.separator + "kad" + File.separator + "tmp");
+    }
+
+    @Bean("downloadsDirectory")
+    @ConditionalOnMissingBean(name = "downloadsDirectory")
+    @ConditionalOnProperty(prefix = "configuration.download", name = "directory")
+    public String downloadsDirectoryFromProperties() {
+        return withLogging(System.getProperty("configuration.download.directory"));
+    }
+
+    @ConditionalOnMissingBean(name = "downloadsDirectory")
+    @Bean("downloadsDirectory")
+    public String defaultDownloadsDirectory() {
+        return withLogging(System.getProperty("user.dir") + File.separator + "tmp");
+    }
+
+    private String withLogging(String directory) {
+        log.info("downloadsDirectory: {}", directory);
+        return directory;
     }
 }
